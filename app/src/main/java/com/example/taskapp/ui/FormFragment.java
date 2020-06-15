711b33da7fb3a4fbd7402782a10aad526c70d5c5
Altynbek1987 <hyundai8788@gmail.com>
2020-06-15 22:36:30 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/FormFragment.java
@@ -0,0 +1,49 @@
+package com.example.taskapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.example.taskapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FormFragment extends Fragment {
+
+    private EditText editTitle;
+    private EditText editDesc;
+
+    public FormFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_form, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editTitle = view.findViewById(R.id.editTitle);
+        editDesc = view.findViewById(R.id.editDesc);
+        view.findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
